3a28e0b0dacbc96f36f542c0e5df5fab32bcaa54
dskarthick97 <dskarthicksabari@gmail.com>
2022-01-21 21:51:34 +0530

    Complete assignment-01 -- values and variables.

---
+++ javascript/assignments/01-fundamentals-part-1/values_and_variables.js
@@ -0,0 +1,13 @@
+// Javascript Values and Variables: https://www.w3schools.com/js/js_syntax.asp
+
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "India";
+let continent = "Asia";
+let population = 100;
+
+console.log(country);
+console.log(continent);
+console.log(population);
