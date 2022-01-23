6a2a7d901a8da027da1620e33edcc11544d417c3
dskarthick97 <dskarthicksabari@gmail.com>
2022-01-23 16:13:16 +0530

    basic js files. Have to continue later

--- javascript/01-fundamentals-part-1/script.js
+++ javascript/01-fundamentals-part-1/script.js
@@ -1,3 +1,18 @@
+// 01 - values and variables
 let js = "amazing";
 
+console.log(js);
 console.log(40 + 8 - 23 - 10);
+
+// 02 - datatypes
+let isJavascriptFun = true;
+console.log(isJavascriptFun);
+
+let variableWithoutValue;
+console.log(variableWithoutValue);
+console.log(typeof variableWithoutValue);
+
+// js says typeof null as object. This considers as bug.
+console.log(typeof null);
+
+// 03 - let, var and const
